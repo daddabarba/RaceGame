1e67e615bd817336e986342ddd38ef7425aa25d4
daddabarba <davidebarbieri97@gmail.com>
2019-04-01 23:33:49 +0200

    updated remote using AiInterface sockets

--- remote.py
+++ remote.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import TCPWrapper as tcp
 import socket
+import struct
 
 import sys
 import time
@@ -11,7 +12,10 @@ class Remote:
 		pg.init()
 		pg.display.set_mode((500,500))
 		pg.display.set_caption("controller")
-		self.__client = tcp.Client(port, medium=socket.AF_UNIX)
+
+		self.__action = tcp.Client(port + "_a", medium=socket.AF_UNIX)
+		self.__state = tcp.Client(port + "_s", medium=socket.AF_UNIX)
+		self.__reward = tcp.Client(port + "_r", medium=socket.AF_UNIX)
 
 	def start(self):
 
@@ -32,21 +36,26 @@ class Remote:
 		keys = pg.key.get_pressed()
 
 		if keys[pg.K_RIGHT] and keys[pg.K_UP]:
-			self.__client(5)
+			self.__action(5)
 		elif keys[pg.K_RIGHT] and keys[pg.K_DOWN]:
-			self.__client(6)
+			self.__action(6)
 		elif keys[pg.K_LEFT] and keys[pg.K_UP]:
-			self.__client(7)
+			self.__action(7)
 		elif keys[pg.K_LEFT] and keys[pg.K_DOWN]:
-			self.__client(8)
+			self.__action(8)
 		elif keys[pg.K_RIGHT]:
-			self.__client(1)
+			self.__action(1)
 		elif keys[pg.K_UP]:
-			self.__client(2)
+			self.__action(2)
 		elif keys[pg.K_LEFT]:
-			self.__client(3)
+			self.__action(3)
 		elif keys[pg.K_DOWN]:
-			self.__client(4)
+			self.__action(4)
+
+		self.__state(1)
+		self.__reward(1)
+
+		print("s: " + str(int.from_bytes(self.__state.get(), "little")) + "\t r: " + str(struct.unpack('f', self.__reward.get())))
 
 
 def main(argv):
