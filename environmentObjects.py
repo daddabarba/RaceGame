70969c66880ef50f0501eb5d954ff65306ec8bfc
daddabarba <davidebarbieri97@gmail.com>
2019-04-09 05:25:08 +0200

    minor bug solved

--- environmentObjects.py
+++ environmentObjects.py
@@ -209,7 +209,7 @@ class Car(EnvObj):
 		super(Car, self).setWindow(window)
 
 	def setNumStates(self, numStates):
-		self.__stateSocket = AII.StateInterface(self.id, self.base, numStates)
+		self.__stateSocket = AII.StateInterface(self.id, self.base, numStates*4)
 
 	def setEnv(self, env):
 		self.__env = env
