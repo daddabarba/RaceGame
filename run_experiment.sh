dd486d3864def886f33897a6e91ec35aedf2f232
davide <davide@iMac-di-Davide.local>
2019-08-18 18:35:05 +0200

    correction of small typo

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,7 +4,7 @@ for i in $(seq 1 1 $1)
 do
 	python3 raceGame.py ~/serv_$i --n-loops $2 --delay 20 --abs-state 0 6 2 100 &
 	pid_env=$!
-	python3 logHandler.py ~/serv_$i true
+	python3 logHandler.py ~/serv_$i true &
 	pid_log=$!
 	sleep 1
 	python3 Agent.py 729 7 0.75 0.99 100 ~/serv_${i}_0 pars_bl pars_bl
