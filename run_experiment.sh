ab9d6a77f5afcc503f725175b2030a725206818a
davide <davide@iMac-di-Davide.local>
2019-08-18 18:41:57 +0200

    minor changes

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,10 +2,10 @@
 
 for i in $(seq 1 1 $1)
 do
-	python3 raceGame.py ~/serv_$i --n-loops $2 --delay 20 --abs-state 0 6 2 100 &
-	pid_env=$!
 	python3 logHandler.py ~/serv_${i}_log true &
 	pid_log=$!
+	python3 raceGame.py ~/serv_$i --n-loops $2 --delay 20 --abs-state 0 6 2 100 &
+	pid_env=$!
 	sleep 1
 	python3 Agent.py 729 7 0.75 0.99 100 ~/serv_${i}_0 pars_bl pars_bl
 	pid_car=$!
