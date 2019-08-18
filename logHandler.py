077705981335744197c421141897fc71dad9ca66
davide <davide@iMac-di-Davide.local>
2019-08-18 18:40:25 +0200

    minor changes

--- logHandler.py
+++ logHandler.py
@@ -7,19 +7,24 @@ import sys
 serv = tcp.Server(port=sys.argv[1], size=8, block=True, medium=socket.AF_UNIX)
 serv.start()
 
+print("opening log handler at ", sys.argv[1])
+
 R = []
 i = 0
+
+store = (sys.argv[2]=="True" or sys.argv[2]=="true")
 
 while True:
 	try:
 		r = struct.unpack('d', serv.get())[0]
-		print("%d) reward: "%i + str(r))
+		if not store:
+			print("%d) reward: "%i + str(r))
 		i+=1
 
 		R.append(r)
 	except:
 		break
 
-if len(sys.argv)==3 and (sys.argv[2]=="True" or sys.argv[2]=="true"):
+if len(sys.argv)==3 and store:
 	with open(sys.argv[1].split("/")[-1]+".txt", "w") as f:
 		f.write(str(R))
