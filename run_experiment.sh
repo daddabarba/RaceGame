417a2bec85a889575e8deaf85d00159e0919a6e8
daddabarba <davidebarbieri97@gmail.com>
2019-08-19 01:32:23 +0200

    parameters to run experiment with baseline changed

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,7 +7,7 @@ do
 	python3 raceGame.py ~/serv_$i --n-loops $2 --delay 20 --abs-state 0 6 2 100 &
 	pid_env=$!
 	sleep 1
-	python3 Agent.py 729 7 0.75 0.99 100 ~/serv_${i}_0 pars_bl pars_bl
+	python3 Agent.py 729 7 0.75 0.99 10 ~/serv_${i}_0 pars_bl pars_bl
 	pid_car=$!
 
 	wait $pid_env
